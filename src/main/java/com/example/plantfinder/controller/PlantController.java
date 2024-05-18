367fd82bb157cb6358397109d3ec3412f8faadd1
Choi-JY <Choi-JY1107@github.com>
2024-05-18 23:53:04 +0900

    fix: 모든 식물 조회할 때, "all" url 제거

--- src/main/java/com/example/plantfinder/controller/PlantController.java
+++ src/main/java/com/example/plantfinder/controller/PlantController.java
@@ -5,9 +5,12 @@ import java.util.Optional;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.plantfinder.dto.PlantAddRequest;
 import com.example.plantfinder.entity.Plant;
 import com.example.plantfinder.service.PlantService;
 
@@ -20,13 +23,24 @@ public class PlantController {
 
     private final PlantService plantService;
 
-    @GetMapping("/all")
+    @GetMapping
     public List<Plant> getAllPlants() {
-        return plantService.getAllPlants();
+        List<Plant> plantList = plantService.getAllPlants();
+
+        return plantList;
     }
 
     @GetMapping("/{id}")
     public Optional<Plant> getPlantById(@PathVariable final String id) {
-        return plantService.getPlantById(id);
+        Optional<Plant> plant = plantService.getPlantById(id);
+
+        return plant;
+    }
+
+    @PostMapping
+    public String createPlant(@RequestBody final PlantAddRequest plantAddRequest) {
+        String plantId = plantService.savePlant(plantAddRequest);
+
+        return plantId;
     }
 }
